50b01d74f98e45d7db58400154a770f2ead0d851
itzamanjain <aman.044063@tmu.ac.in>
2024-11-11 01:24:35 +0530

    dark light mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar({ cartItemsCount, onCartClick }: NavbarProps) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-gray-900 text-white fixed w-full z-50 top-0">
+    <nav className="bg-white text-black dark:bg-black dark:text-white  fixed w-full z-50 top-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
